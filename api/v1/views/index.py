7db0fd7f42e5dd834527d16e4c1d0dae4ecfcdfd
Alex <asenges400@gmail.com>
2022-09-10 16:01:50 -0300

    Task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """ Index """
-
+import models
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
@@ -9,3 +16,14 @@ from flask import jsonify
 def status():
     """ Returns the status of the API """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """ JSON Response """
+    todo = {'states': State, 'users': User,
+            'amenities': Amenity, 'cities': City,
+            'places': Place, 'reviews': Review}
+    for key in todo:
+        todo[key] = storage.count(todo[key])
+    return jsonify(todo)
