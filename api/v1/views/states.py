6a4df5174324a1de18151e87894cc24276151514
FloLys <florelysmestre@gmail.com>
2022-09-11 22:29:51 +0000

    correcting outputs

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -19,15 +19,15 @@ def states():
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post():
     """ Handles POST request. Creates a new State with status 201, else 400 """
-    if not request.is_json:
+    body_req = request.get_json(force=True, silent=True)
+    if body_req is None:
         abort(400, "Not a JSON")
-    if 'name' not in request.get_json():
+    if 'name' not in body_req:
         abort(400, "Missing name")
-    else:
-        new_state = State(**request.get_json())
-        storage.new(new_state)
-        storage.save()
-        return jsonify(new_state.to_dict()), 201
+    new_state = State(**request.get_json())
+    storage.new(new_state)
+    storage.save()
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
@@ -56,14 +56,12 @@ def put(state_id):
     """ Handles PUT request. Updates a State obj with status 200, else 400 """
     ignore_keys = ['id', 'created_at', 'updated_at']
     obj = storage.get(State, state_id)
+    new_attr = request.get_json(force=True, silent=True)
     if not obj:
         abort(404)
-    if not request.is_json:
+    if new_attr is None:
         abort(400, "Not a JSON")
-    if 'name' not in request.get_json():
-        abort(400, "Missing name")
     else:
-        new_attr = request.get_json()
         for key, value in new_attr.items():
             if key in ignore_keys:
                 continue
