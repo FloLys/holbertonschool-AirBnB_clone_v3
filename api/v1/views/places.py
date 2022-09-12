3e77c008d803e0c0d9c789da5ba360e272d85055
FloLys <florelysmestre@gmail.com>
2022-09-12 02:38:04 +0000

    places correct

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,39 +1,42 @@
 #!/usr/bin/python3
 """ Handles RESTful API actions """
 from api.v1.views import app_views
-from flask import jsonify, abort, request, make_response
+from flask import jsonify, abort, request
 from models import storage
+from models.state import State
+from models.city import City
 from models.place import Place
 
 
 @app_views.route('/places', methods=['GET'], strict_slashes=False)
-def places():
+def all_places_of_city():
     """ Returns all objects """
     all_objs = []
-    objs = storage.all(State)
+    objs = storage.all(Place)
     for obj in objs.values():
         all_objs.append(obj.to_dict())
     return jsonify(all_objs)
 
 
-@app_views.route('/places', methods=['POST'], strict_slashes=False)
-def post_places():
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def post_place(city_id):
     """ Handles POST request. Creates a new entry with status 201, else 400 """
     body_req = request.get_json(force=True, silent=True)
     if body_req is None:
-        abort(400, description="Not a JSON")
+        abort(400, "Not a JSON")
     if 'name' not in body_req:
-        abort(400, description="Missing name")
-    new_state = State(**request.get_json())
-    storage.new(new_state)
+        abort(400, "Missing name")
+    new_place = Place(**request.get_json())
+    storage.new(new_place)
     storage.save()
-    return make_response(jsonify(storage.to_dict()), 201)
+    return jsonify(new_place.to_dict()), 201
 
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
-def get_places(place_id):
+def get_place(place_id):
     """ Handles GET request. Returns the obj to dictionary else raise 404 """
-    obj = storage.get(State, place_id)
+    obj = storage.get(Place, place_id)
     if obj:
         return jsonify(obj.to_dict())
     abort(404)
@@ -41,30 +44,30 @@ def get_places(place_id):
 
 @app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
-def delete_places(place_id):
+def delete_place(place_id):
     """ Handles DELETE request. Returns empty dictionary else raise 404 err """
-    obj = storage.get(State, place_id)
+    obj = storage.get(Place, place_id)
     if obj:
         storage.delete(obj)
         storage.save()
-        return make_response(jsonify({}), 200)
+        return {}, 200
     abort(404)
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-def put_places(place_id):
+def put_place(place_id):
     """ Handles PUT request. Updates the obj with status 200, else 400 """
-    ignore_keys = ['id', 'created_at', 'updated_at']
-    obj = storage.get(State, place_id)
+    ignore_keys = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
+    obj = storage.get(Place, place_id)
     new_attr = request.get_json(force=True, silent=True)
     if not obj:
         abort(404)
     if new_attr is None:
-        abort(400, description="Not a JSON")
+        abort(400, "Not a JSON")
     else:
         for key, value in new_attr.items():
             if key in ignore_keys:
                 continue
             setattr(obj, key, value)
         storage.save()
-        return make_response(jsonify(state.to_dict()), 200)
+        return jsonify(obj.to_dict()), 200
